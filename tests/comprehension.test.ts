2d3363f20a817762863047775122a9eff6144aef
shreyasaha1997 <shreyasaha25@gmail.com>
2022-05-13 12:59:26 -0700

    corrected failure test cases

--- tests/comprehension.test.ts
+++ tests/comprehension.test.ts
@@ -5,67 +5,97 @@ describe("List Comprehension Tests", () => {
 //havent properly written next and hasnext yet
   assertTCFail("invalid expression in comprehension", `
 class range(object):
-    a:int=9
-    b:int=10
-    def __init__(self:range, a:int, b:int):
-        self.a=a,self.b=b
-    def next(q:int,w:int)->int:
-        return 3
-    def hasNext(q:int,w:int)->int:
-        return 3
+    curr:int=0
+    min:int=0
+    max:int=10
+    def __init__(self:range, min:int, max:int):
+        self.curr=min
+        self.min=min
+        self.max=max
+    def next(self:range)->int:
+        return self.curr+1
+    def hasNext(self:range)->bool:
+        if self.curr<self.max:
+            return True
+        else:
+            return False
 print([j for a in range(1,2) if 1<2])
   `);
 
 
 assertTCFail("invalid range in comprehension", `
 class range(object):
-    a:int=9
-    b:int=10
-    def __init__(self:range, a:int, b:int):
-        self.a=a,self.b=b
-    def next(q:int,w:int)->int:
-        return 3
-    def hasNext(q:int,w:int)->int:
-        return 3
+    curr:int=0
+    min:int=0
+    max:int=10
+    def __init__(self:range, min:int, max:int):
+        self.curr=min
+        self.min=min
+        self.max=max
+    def next(self:range)->int:
+        return self.curr+1
+    def hasNext(self:range)->bool:
+        if self.curr<self.max:
+            return True
+        else:
+            return False
 print([j for a in range(2,1) if 1<2])
   `);
   
 assertTCFail("Not if in comprehension", `
 class range(object):
-    a:int=9
-    b:int=10
-    def __init__(self:range, a:int, b:int):
-        self.a=a,self.b=b
-    def next(q:int,w:int)->int:
-        return 3
-    def hasNext(q:int,w:int)->int:
-        return 3
+    curr:int=0
+    min:int=0
+    max:int=10
+    def __init__(self:range, min:int, max:int):
+        self.curr=min
+        self.min=min
+        self.max=max
+    def next(self:range)->int:
+        return self.curr+1
+    def hasNext(self:range)->bool:
+        if self.curr<self.max:
+            return True
+        else:
+            return False
 print([j for a in range(1,2) for 1<2])
 `);
   
 assertTCFail("invalid condition in comprehension", `
 class range(object):
-    a:int=9
-    b:int=10
-    def __init__(self:range, a:int, b:int):
-        self.a=a,self.b=b
-    def next(q:int,w:int)->int:
-        return 3
-    def hasNext(q:int,w:int)->int:
-        return 3
+    curr:int=0
+    min:int=0
+    max:int=10
+    def __init__(self:range, min:int, max:int):
+        self.curr=min
+        self.min=min
+        self.max=max
+    def next(self:range)->int:
+        return self.curr+1
+    def hasNext(self:range)->bool:
+        if self.curr<self.max:
+            return True
+        else:
+            return False
 print([j for a in range(1,2) if 1+2])
   `);
 
 assertTCFail("invalid iterable in comprehension", `
 class range(object):
-    a:int=9
-    b:int=10
-    def __init__(self:range, a:int, b:int):
-        self.a=a,self.b=b
-    def next(q:int,w:int)->int:
-        return 3
-    def hasNext(q:int,w:int)->int:
-        return 3
+    curr:int=0
+    min:int=0
+    max:int=10
+    def __init__(self:range, min:int, max:int):
+        self.curr=min
+        self.min=min
+        self.max=max
+    def next(self:range)->int:
+        return self.curr+1
+    def hasNext(self:range)->bool:
+        if self.curr<self.max:
+            return True
+        else:
+            return False
 print([j for a in A if 1<2])
 `);
 
